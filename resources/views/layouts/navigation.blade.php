f3736e69d1ecc27018f40438b20c5e6a7d512fd1
Juan Diego <perezlopezjuandiego59@gmail.com>
2026-06-08 13:16:22 -0500

    agregar Procfile para Railway

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -18,13 +18,13 @@
                         @if(Auth::user()->isAdmin())
                             <x-dropdown align="left" width="48">
                                 <x-slot name="trigger">
-                                    <button class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none transition duration-150 ease-in-out">
+                                    <button class="inline-flex items-center h-16 px-1 border-b-2 border-transparent text-sm font-medium text-gray-500 hover:text-white-700 hover:border-gray-300 focus:outline-none transition duration-150 ease-in-out">
                                         Admin
                                         <svg class="ms-1 fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
                                             <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                         </svg>
-                                    </button>
-                                </x-slot>
+                                </button>
+                            </x-slot>
                                 <x-slot name="content">
                                     <x-dropdown-link :href="route('admin.venues.index')">Complejos</x-dropdown-link>
                                     <x-dropdown-link :href="route('admin.fields.index')">Canchas</x-dropdown-link>
