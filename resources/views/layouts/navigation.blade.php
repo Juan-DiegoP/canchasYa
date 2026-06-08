57913982d1b53b2c15df1c22ecd78f8981306d4c
Juan Diego <perezlopezjuandiego59@gmail.com>
2026-06-07 20:59:31 -0500

    build assets para produccion

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,9 +16,23 @@
                     </x-nav-link>
                     @auth
                         @if(Auth::user()->isAdmin())
-                            <x-nav-link :href="route('admin.venues.index')" :active="request()->is('admin/*')">
-                                Admin
-                            </x-nav-link>
+                            <x-dropdown align="left" width="48">
+                                <x-slot name="trigger">
+                                    <button class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none transition duration-150 ease-in-out">
+                                        Admin
+                                        <svg class="ms-1 fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                        </svg>
+                                    </button>
+                                </x-slot>
+                                <x-slot name="content">
+                                    <x-dropdown-link :href="route('admin.venues.index')">Complejos</x-dropdown-link>
+                                    <x-dropdown-link :href="route('admin.fields.index')">Canchas</x-dropdown-link>
+                                    <x-dropdown-link :href="route('admin.sport-types.index')">Deportes</x-dropdown-link>
+                                    <x-dropdown-link :href="route('admin.reservations.index')">Reservas</x-dropdown-link>
+                                    <x-dropdown-link :href="route('admin.reservations.ocupacion')">Ocupación</x-dropdown-link>
+                                </x-slot>
+                            </x-dropdown>
                         @endif
                         <x-nav-link :href="route('reservations.index')" :active="request()->routeIs('reservations.*')">
                             Mis reservas
@@ -41,7 +55,6 @@
                                 </div>
                             </button>
                         </x-slot>
-
                         <x-slot name="content">
                             <x-dropdown-link :href="route('profile.edit')">
                                 {{ __('Profile') }}
@@ -82,7 +95,19 @@
             @auth
                 @if(Auth::user()->isAdmin())
                     <x-responsive-nav-link :href="route('admin.venues.index')" :active="request()->is('admin/*')">
-                        Admin
+                        Complejos
+                    </x-responsive-nav-link>
+                    <x-responsive-nav-link :href="route('admin.fields.index')">
+                        Canchas (admin)
+                    </x-responsive-nav-link>
+                    <x-responsive-nav-link :href="route('admin.sport-types.index')">
+                        Deportes
+                    </x-responsive-nav-link>
+                    <x-responsive-nav-link :href="route('admin.reservations.index')">
+                        Reservas
+                    </x-responsive-nav-link>
+                    <x-responsive-nav-link :href="route('admin.reservations.ocupacion')">
+                        Ocupación
                     </x-responsive-nav-link>
                 @endif
                 <x-responsive-nav-link :href="route('reservations.index')" :active="request()->routeIs('reservations.*')">
